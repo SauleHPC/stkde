3761c084d47e615fded25d325cd58566816df8b4
Erik Saule <esaule@cph-i1.uncc.edu>
2016-10-15 19:04:58 -0400

    adding memory

--- src/run_one.sh
+++ src/run_one.sh
@@ -11,7 +11,7 @@ echo $BOUNDARY
 echo $OBSERVATIONS
 echo $BANDWIDTH
 
-for method in POINTBASED POINTBASED-SYMDISK POINTBASED-SYMBAR POINTBASED-SYM POINTBASED-SYMOMP
+for method in POINTBASED POINTBASED-SYMDISK POINTBASED-SYMBAR POINTBASED-SYM POINTBASED-SYMOMP POINTBASED-SYMOMP-POINTDECOMP
 do
     if [ $method = POINTBASED-SYMOMP ]
     then
@@ -34,12 +34,25 @@ do
 	    done
 	done
     else
-	FILE=results/${INSTANCE}_$(basename $BANDWIDTH)_${method}
-	if [ ! -f $FILE ]
+
+	if [ $method = POINTBASED-SYMOMP-POINTDECOMP ]
 	then
-	    echo $BIN $BOUNDARY $OBSERVATIONS $BANDWIDTH $method
-	    $BIN $BOUNDARY $OBSERVATIONS $BANDWIDTH $method > $FILE 2>&1 
+	    for t in 1 2 4 8 16
+	    do
+		FILE=results/${INSTANCE}_$(basename $BANDWIDTH)_${method}_t${t}
+		if [ ! -f $FILE ]
+		then
+		  echo OMP_NUM_THREADS=$t  $BIN $BOUNDARY $OBSERVATIONS $BANDWIDTH $method 
+		  OMP_NUM_THREADS=$t  $BIN $BOUNDARY $OBSERVATIONS $BANDWIDTH $method > $FILE 2>&1
+		fi
+	    done
+	else
+	    FILE=results/${INSTANCE}_$(basename $BANDWIDTH)_${method}
+	    if [ ! -f $FILE ]
+	    then
+		echo $BIN $BOUNDARY $OBSERVATIONS $BANDWIDTH $method
+		$BIN $BOUNDARY $OBSERVATIONS $BANDWIDTH $method > $FILE 2>&1 
+	    fi
 	fi
     fi
-
 done
