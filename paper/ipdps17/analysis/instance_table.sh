633244fb9f00caedc95a943238ef7b149cc7587a
Erik Saule <esaule@uncc.edu>
2016-10-21 12:46:59 -0400

    dataset description

--- paper/ipdps17/analysis/instance_table.sh
+++ paper/ipdps17/analysis/instance_table.sh
@@ -7,15 +7,29 @@ INSTANCES="Dengue_lowres-lowbw Dengue_lowres-highbw Dengue_highres-lowbw Dengue_
 RESULTDIR=../results
 
 (
-    echo instance \& N \& Grid \& Size \& SBW \& TBW  '\\\\'
+    echo '\\begin{tabular}{|l|rrr|rr|}'
+    echo \\hline
+    echo Instance \& N \& Grid \& Size \& SBW \& TBW  '\\\\'
+    echo \\hline
     for inst in $INSTANCES
     do
 	FILE=${RESULTDIR}/${inst}_POINTBASED
 	echo $inst \& \
 	     $(grep read $FILE | grep observations | cut -d \   -f 2) \& \
 	     $(grep voxsize: $FILE | cut -d \  -f 2) \& \
-	     $(grep voxsize: $FILE | cut -d : -f 3) \& \
+	     $(grep voxsize: $FILE | cut -d : -f 3 | awk '{printf "%dMB", $1}') \& \
 	     $(grep voxBW: $FILE | cut -d \   -f 2) \& \
 	     $(grep voxBW: $FILE | cut -d \   -f 3)  '\\\\'
     done
-) | sed 's/_/\\_/g' > instance_table.tex
+    echo \\hline
+    echo '\\end{tabular}'
+) | sed 's/-Animal//' \
+  | sed 's/lowres/Lr/g' \
+  | sed 's/medres/Mr/g' \
+  | sed 's/highres/Hr/g' \
+  | sed 's/lowbw/Lb/g' \
+  | sed 's/medbw/Mb/g' \
+  | sed 's/highbw/Hb/g' \
+  | sed 's/veryhighbandwidth/VHb/g' \
+  | sed 's/_/\\_/g' \
+  > instance_table.tex
