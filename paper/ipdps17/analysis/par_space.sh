87cfc9b2647d76e181ac710c2729d0d092a115ae
Erik Saule <esaule@uncc.edu>
2016-10-22 01:46:38 -0400

    more plots

--- paper/ipdps17/analysis/par_space.sh
+++ paper/ipdps17/analysis/par_space.sh
@@ -121,12 +121,12 @@ DECOMP="1_1_1 2_2_2 4_4_4 8_8_8 16_16_16 32_32_32 64_64_64"
     | sed 's/medbw/Mb/g' \
     | sed 's/highbw/Hb/g' \
     | sed 's/veryhighbandwidth/VHb/g' \
-    | sed 's/_/\\_/g' > SYM-DD-speedup.data
+    | sed 's/_/\\_/g' > SYM-DD-overhead.data
 
 
 gnuplot<<EOF
 set terminal pdf size 10in,3in 
-set output 'SYM-DD.pdf'
+set output 'SYM-DD-overhead.pdf'
 
 set key font ",15"
 set xtics font ",15"
@@ -141,7 +141,7 @@ set xrange [-.5:12.5]
 set yrange [0:*]
 set ylabel 'Time relative to PB-SYM'
 
-plot 'SYM-DD-speedup.data' using 2:xtic(1) title col, \
+plot 'SYM-DD-overhead.data' using 2:xtic(1) title col, \
         '' using 3:xtic(1) title col, \
         '' using 4:xtic(1) title col, \
         '' using 5:xtic(1) title col, \
@@ -150,6 +150,71 @@ plot 'SYM-DD-speedup.data' using 2:xtic(1) title col, \
         '' using 8:xtic(1) title col
 
 EOF
+
+
+#clustered bar chart for 16 threads.
+
+(
+    echo -n Instance \ 
+    for dec in $DECOMP
+    do
+	echo -n ${dec} \  
+    done |tr _ x
+
+    for inst in $INSTANCES
+    do
+	seq=$(gettime ${inst} POINTBASED-SYM)
+	t=16
+	
+	echo -n ${inst} \  
+
+	for decomp in $DECOMP
+	do
+	    meth=POINTBASED-SYMOMP_${decomp}_t${t}
+	    
+	    tim=$(gettime ${inst} ${meth})
+	    echo -n $(echo ${seq}/${tim}  | bc -l) \ 
+	done
+	echo
+    done
+) | sed 's/-Animal//' \
+    | sed 's/lowres/Lr/g' \
+    | sed 's/medres/Mr/g' \
+    | sed 's/highres/Hr/g' \
+    | sed 's/lowbw/Lb/g' \
+    | sed 's/medbw/Mb/g' \
+    | sed 's/highbw/Hb/g' \
+    | sed 's/veryhighbandwidth/VHb/g' \
+    | sed 's/_/\\_/g' > SYM-DD-speedup16.data
+
+
+gnuplot<<EOF
+set terminal pdf size 10in,3in 
+set output 'SYM-DD-speedup16.pdf'
+
+set key font ",15"
+set xtics font ",15"
+set ytics font ",15"
+set ylabel font ",15"
+set style data histogram
+set style histogram cluster gap 2
+
+set xtics rotate by -45
+set style fill solid border rgb "black"
+set xrange [-.5:12.5]
+set yrange [0:*]
+set ylabel 'Speedup'
+
+plot 'SYM-DD-speedup16.data' using 2:xtic(1) title col, \
+        '' using 3:xtic(1) title col, \
+        '' using 4:xtic(1) title col, \
+        '' using 5:xtic(1) title col, \
+        '' using 6:xtic(1) title col, \
+        '' using 7:xtic(1) title col, \
+        '' using 8:xtic(1) title col
+
+EOF
+
 
 exit
 
