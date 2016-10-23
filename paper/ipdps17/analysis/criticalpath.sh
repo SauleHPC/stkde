00f1124c3f31468c7b20c840016737dfbf167510
Erik Saule <esaule@uncc.edu>
2016-10-23 00:05:27 -0400

    experimental results

---
+++ paper/ipdps17/analysis/criticalpath.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+cpobs() {
+    instance=$1
+
+    grep maxin\ round ../results/${instance}_POINTBASED-SYMOMP-OBSDECOMP_64_64_64_t16 | cut -d: -f 2 | awk '{sum += $1;} END{print sum}'
+}
+
+cpcolorsched() {
+    instance=$1
+
+    grep load\ based\ longest\ path ../results/${instance}_POINTBASED-SYMOMP-OBSDECOMP-COLORSCHED_64_64_64_t16 | cut -d: -f 2 | awk '{sum += $1;} END{print sum}'
+
+}
+
+nbpoint() {
+    instance=$1
+
+    grep read ../results/${instance}_POINTBASED-SYM | grep observations | cut -d \   -f 2 
+}
+
+INSTANCES="Dengue_lowres-lowbw Dengue_lowres-highbw Dengue_highres-lowbw Dengue_highres-highbw Dengue_highres-veryhighbandwidth \
+            Pollen_lowres-lowbw Pollen_highres-lowbw Pollen_highres-medbw Pollen_highres-highbw \
+	    Flu-Animal_lowres-lowbw Flu-Animal_lowres-highbw Flu-Animal_medres-lowbw Flu-Animal_medres-highbw Flu-Animal_highres-lowbw Flu-Animal_highres-highbw"
+
+(
+    echo instance PB-SYM-PD PB-SYM-PD-SCHED
+
+    for instance in ${INSTANCES}
+    do
+	echo $instance \
+	     $(echo $(cpobs $instance) / $(nbpoint $instance) | bc -l) \
+	     $(echo $(cpcolorsched $instance) / $(nbpoint $instance) | bc -l)
+    done  | sed 's/-Animal//' \
+    | sed 's/lowres/Lr/g' \
+    | sed 's/medres/Mr/g' \
+    | sed 's/highres/Hr/g' \
+    | sed 's/lowbw/Lb/g' \
+    | sed 's/medbw/Mb/g' \
+    | sed 's/highbw/Hb/g' \
+    | sed 's/veryhighbandwidth/VHb/g' \
+    | sed 's/_/\\_/g'
+    
+)> PB-SYM-PD-imbalance.data
+
+gnuplot<<EOF
+
+set terminal pdf
+set output 'PB-SYM-PD-imbalance.pdf'
+
+set xtic rotate by -45
+
+set ylabel 'relative length of the critical path'
+
+plot 'PB-SYM-PD-imbalance.data' u 2:xtic(1) title col, \
+     '' u 3:xtic(1) title col lc 3
+
+EOF
--- paper/ipdps17/analysis/par_col.sh
+++ paper/ipdps17/analysis/par_col.sh
@@ -60,3 +60,71 @@ DECOMP="1_1_1 2_2_2 4_4_4 8_8_8 16_16_16 32_32_32 64_64_64"
 )  >  parcol-t16.tex
 
 
+#speedup bar chart
+(
+    echo -n Instance \ 
+    for dec in $DECOMP
+    do
+	echo -n ${dec} \  
+    done |tr _ x
+    echo
+    
+    for inst in $INSTANCES
+    do
+	seq=$(gettime ${inst} POINTBASED-SYM)
+	t=16
+	
+	echo -n ${inst} \  
+
+	for decomp in $DECOMP
+	do
+	    meth=POINTBASED-SYMOMP-OBSDECOMP_${decomp}_t${t}
+	    
+	    tim=$(gettime ${inst} ${meth})
+	    if [ "$tim" = "0" ]
+	    then
+		echo -n 0 \ 
+	    else
+		echo -n $(echo ${seq}/${tim}  | bc -l) \ 
+	    fi
+	done
+	echo
+    done
+) | sed 's/-Animal//' \
+    | sed 's/lowres/Lr/g' \
+    | sed 's/medres/Mr/g' \
+    | sed 's/highres/Hr/g' \
+    | sed 's/lowbw/Lb/g' \
+    | sed 's/medbw/Mb/g' \
+    | sed 's/highbw/Hb/g' \
+    | sed 's/veryhighbandwidth/VHb/g' \
+    | sed 's/_/\\_/g' > SYM-PD-speedup16.data
+
+
+gnuplot<<EOF
+set terminal pdf size 10in,3in 
+set output 'SYM-PD-speedup16.pdf'
+
+set key font ",15"
+set xtics font ",15"
+set ytics font ",15"
+set ylabel font ",15"
+set style data histogram
+set style histogram cluster gap 2
+
+set xtics rotate by -45
+set style fill solid border rgb "black"
+set xrange [-.5:14.5]
+set yrange [0:18]
+set ylabel 'Speedup'
+
+plot 'SYM-PD-speedup16.data' using 2:xtic(1) title col, \
+        '' using 3:xtic(1) title col, \
+        '' using 4:xtic(1) title col, \
+        '' using 5:xtic(1) title col, \
+        '' using 6:xtic(1) title col, \
+        '' using 7:xtic(1) title col, \
+        '' using 8:xtic(1) title col
+
+EOF
+
