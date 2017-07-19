0103a0c07daa6dfaddfaf901fc0bb54173b14d55
Tahmid Efaz <tefaz@cph-i2.uncc.edu>
2017-07-19 12:19:45 -0400

    Makefile updated

---
+++ src/job.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# ===== PBS OPTIONS =====
+### Set the job name
+#PBS -N "CUDA-test"
+
+### Specify queue to run in
+#PBS -q copperhead
+
+### Specify number of CPUs for job
+#PBS -l nodes=1:ppn=1:gpus=1
+
+
+# ==== Main ======
+### make /users/tefaz/stkde/src
+./point-based ../dataset/Dengue-allcases-2010-2011/AllCases2010_11_clip_bds.txt  ../dataset/Dengue-allcases-2010-2011/AllCases2010_11_clip.txt  ../dataset/Dengue-allcases-2010-2011/lowres-lowbw POINTBASED
