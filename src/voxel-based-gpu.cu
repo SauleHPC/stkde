7d3537726ec46873623496c40dc0cb0381a21d93
Tahmid Efaz <tefaz@cph-i1.uncc.edu>
2017-07-24 03:32:49 -0400

    Voxel based GPU implementation

--- src/voxel-based-gpu.cu
+++ src/voxel-based-gpu.cu
@@ -3,32 +3,46 @@
 #include <iostream>
 #include "types.hpp"
 #include "io.hpp"
-#include "density.hpp"
+
 #include <memory>
 #include "compact.hpp"
 
 #define N 5000
 
-__device__ int function(int m, int n){
+__device__ values densityF(coordinate obsX, coordinate obsY, coordinate obsT,
+		 coordinate voxX, coordinate voxY, coordinate voxT,
+		 int nbObs, coordinate xbw, coordinate tbw
+		 ){
 
-  return(m+n);
+  values u = (obsX-voxX) / xbw;
+  values v = (obsY-voxY) / xbw;
+  values w = (obsT-voxT) / tbw;
+    
+  values constantTerm = 1000.*1000.*1000.*10 / (nbObs * pow(xbw, 2) * tbw);
+  values Ks = (0.5 * M_PI) * (1 - pow(u, 2) - pow(v, 2));
+  values Kt = 0.75 * (1 - pow(w, 2));
+  
+  values spaceTimeKDE = constantTerm * Ks * Kt;
+    
+  return spaceTimeKDE;
 
 }
 
-__global__ void densityG(char* f, int n){
-
-//..
-//	function(f,n);
-  
-  
+__global__ void voxel_gpu(value *val, long int *ev,coordinate *xx,coordinate *yy,coordinate *tt,coordinate *obxx,coordinate *obyy,coordinate *obtt,coordinate *xw,coordinate *tw){
     
-  //int id = threadIdx.x + blockIdx.x * blockDim.x;
-  //if (id < n)
-    //c[id] = a[id] + b[id];
-    //c[id]= function(a[id],b[id]);
-    
-//..
+  int id = threadIdx.x + blockIdx.x * blockDim.x;
 
+  coordinate ox = obxx[id];
+  coordinate oy = obyy[id];
+  coordinate ot = obtt[id];	  
+  if(std::abs(tt - ot) <= tw) {
+    if(std::sqrt((xx - ox) * (xx - ox) + (yy - oy) * (yy - oy)) <= xw) {
+      val += densityF(xx, yy, tt,
+			      ox, oy, ot,
+			      id, xw, tw);
+      ev++;
+    }
+  }
 }
 
 
@@ -61,21 +75,51 @@ std::shared_ptr<util::Compact3D<values>> stkde_voxelbased_gpu(const bounding_box
 
 	values v = 0;
  
-  // do every observation point
-	for(int o = 0; o < n; o++) {
-	  coordinate ox = inst.obsx[o];
-	  coordinate oy = inst.obsy[o];
-	  coordinate ot = inst.obst[o];	  
-	  if(std::abs(pt - ot) <= pa.tbw) {
-	    if(std::sqrt((px - ox) * (px - ox) + (py - oy) * (py - oy)) <= pa.xbw) {
-	      v += densityF(px, py, pt,
-				      ox, oy, ot,
-				      n, pa.xbw, pa.tbw);
-	      evals++;
-	    }
-	  }
-	}
+ coordinate *d_px, *d_py, *d_pt;
+ coordinate *d_obsx[n], *d_obsy[n], *d_obst[n];
  
+ // device copy of v
+ cudaMalloc((void**)&d_v, sizeof(values));
+ 
+ // device copy of the points
+ cudaMalloc((void**)&d_px, sizeof(coordinate));
+ cudaMalloc((void**)&d_py, sizeof(coordinate));
+ cudaMalloc((void**)&d_pt, sizeof(coordinate));
+ 
+ //device copy of the observation points
+ cudaMalloc((void**)&d_obsx, n * sizeof(coordinate));
+ cudaMalloc((void**)&d_obsy, n * sizeof(coordinate));
+ cudaMalloc((void**)&d_obst, n * sizeof(coordinate));
+ 
+ //device copy of pa.xbw and pa.tbw
+ cudaMalloc((void**)&d_pax, sizeof(coordinate));
+ cudaMalloc((void**)&d_pat, sizeof(coordinate));
+ 
+ //device copy of evals
+ cudaMalloc((void**)&d_e, sizeof(long int));
+ 
+ //Copying to the GPU
+ 
+ cudaMemcpy( d_v, v, sizeof(values), cudaMemcpyHostToDevice);
+ 
+ cudaMemcpy( d_px, px, sizeof(coordinate), cudaMemcpyHostToDevice);
+ cudaMemcpy( d_py, py, sizeof(coordinate), cudaMemcpyHostToDevice);
+ cudaMemcpy( d_pt, pt, sizeof(coordinate), cudaMemcpyHostToDevice);
+
+ cudaMemcpy( d_obx, inst.obsx, n * sizeof(coordinate), cudaMemcpyHostToDevice);
+ cudaMemcpy( d_oby, inst.obsy, n * sizeof(coordinate), cudaMemcpyHostToDevice);
+ cudaMemcpy( d_obt, inst.obst, n * sizeof(coordinate), cudaMemcpyHostToDevice);
+ 
+ cudaMemcpy( d_pax, pa.xbw, sizeof(coordinate), cudaMemcpyHostToDevice);
+ cudaMemcpy( d_pat, pa.tbw, sizeof(coordinate), cudaMemcpyHostToDevice);
+ 
+ cudaMemcpy( d_e, evals, sizeof(long int), cudaMemcpyHostToDevice);
+
+ voxel_gpu<<<(n+127)/128,128>>>(d_v, d_e, d_px, d_py, d_pt, d_obx, d_oby, d_obt, d_pax, d_pat); 
+
+ //Copying from GPU to CPU
+ cudaMemcpy( v, d_v, sizeof(values), cudaMemcpyDeviceToHost);
+ cudaMemcpy( evals, d_c, sizeof(long int), cudaMemcpyDeviceToHost);
  
   co(i, j, k) = v;
      }
