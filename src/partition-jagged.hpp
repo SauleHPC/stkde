3fb5f82b21a17203cca2e04e28a5ed59e43560ad
Erik Saule <esaule@uncc.edu>
2017-10-21 12:43:31 -0400

    jagged XY

--- src/partition-jagged.hpp
+++ src/partition-jagged.hpp
@@ -19,7 +19,6 @@ namespace stkde{
     }
   };
 
-  
   struct jagged_level_two_key {
     index xmin, xmax;
     int nbpart;    
@@ -40,6 +39,68 @@ namespace stkde{
 
   };
 
+  struct jagged_level_three_key {
+    index xmin, xmax;
+    index ymax;
+    int nbpart;    
+
+    bool operator<(const jagged_level_three_key& k2) const {
+      if (this->xmax < k2.xmax)
+	return true;
+      if (this->xmax > k2.xmax)
+	return false;
+      
+      if (this->xmin < k2.xmin)
+	return true;
+      if (this->xmin > k2.xmin)
+	return false;
+
+      if (this->ymax < k2.ymax)
+	return true;
+      if (this->ymax > k2.ymax)
+	return false;
+
+      
+      return this->nbpart < k2.nbpart;
+    }
+
+  };
+
+  
+  struct jagged_level_four_key {
+    index xmin, xmax;
+    index ymin, ymax;
+    int nbpart;    
+
+    bool operator<(const jagged_level_four_key& k2) const {
+      if (this->xmax < k2.xmax)
+	return true;
+      if (this->xmax > k2.xmax)
+	return false;
+      
+      if (this->xmin < k2.xmin)
+	return true;
+      if (this->xmin > k2.xmin)
+	return false;
+
+      if (this->ymax < k2.ymax)
+	return true;
+      if (this->ymax > k2.ymax)
+	return false;
+
+      if (this->ymin < k2.ymin)
+	return true;
+      if (this->ymin > k2.ymin)
+	return false;
+
+      
+      return this->nbpart < k2.nbpart;
+    }
+
+  };
+
+  
+  
   struct dp_jagged_parameters: public dp_parameters {
 
     index xstep;
@@ -50,12 +111,157 @@ namespace stkde{
       :dp_parameters(co)
     {}
 
-    std::map<jagged_level_one_key, dp_hier_val> memo_l1;
-    std::map<jagged_level_two_key, dp_hier_val> memo_l2;
+    std::map<jagged_level_one_key, dp_hier_val> memo_l1; // an xcut cube
+    std::map<jagged_level_two_key, dp_hier_val> memo_l2; //a full X slice
+    std::map<jagged_level_three_key, dp_hier_val> memo_l3; //an xslice ycut
+    std::map<jagged_level_four_key, dp_hier_val> memo_l4; //a full XY cut
     size_t nbstate;
   };
 
 
+  dp_hier_val partition_jaggedZ(dp_jagged_parameters& param,
+				const stkde::instance& inst,
+				index Xmin,
+				index Xmax,
+				index Ymin,
+				index Ymax,
+				int nbparts,
+				double maxloadallowed) {
+    const computation& c = param.c;
+
+    jagged_level_four_key k;
+    k.xmin = Xmin;
+    k.xmax = Xmax;
+    k.ymin = Ymin;
+    k.ymax = Ymax;
+    k.nbpart = nbparts;
+    
+    auto it = param.memo_l4.find(k);
+
+    if (it != param.memo_l4.end()) {
+      return it->second;
+    }
+
+    dp_hier_val ret;
+    
+    //naive solution: one box
+    {
+      stkde::voxelbox vb(Xmin, Xmax,
+			 Ymin, Ymax,
+			 0, c.voxT);
+          
+      ret.sol.push_back(vb);
+      ret.maxload = cost_of_box(param, vb, inst);
+      ret.sumload = ((ret.maxload <= maxloadallowed)?ret.maxload : std::numeric_limits<double>::infinity());
+    }
+
+    
+    //save solution and return it
+    param.memo_l4[k] = ret;
+    
+    return ret;    
+  }
+
+  
+  
+
+  dp_hier_val partition_jaggedY_over_rec(dp_jagged_parameters& param,
+					 const stkde::instance& inst,
+					 index Xmin,
+					 index Xmax,
+					 index Ymax,
+					 int nbparts,
+					 double maxloadallowed) {
+    const computation& c = param.c;
+
+    jagged_level_three_key k;
+    k.xmin = Xmin;
+    k.xmax = Xmax;
+    k.ymax = Ymax;
+    k.nbpart = nbparts;
+    
+    auto it = param.memo_l3.find(k);
+
+    if (it != param.memo_l3.end()) {
+      return it->second;
+    }
+
+    dp_hier_val ret;
+    
+    //naive solution: one box
+    {
+      stkde::voxelbox vb(Xmin, Xmax,
+			 0, Ymax,
+			 0, c.voxT);
+          
+      ret.sol.push_back(vb);
+      ret.maxload = cost_of_box(param, vb, inst);
+      ret.sumload = ((ret.maxload <= maxloadallowed)?ret.maxload : std::numeric_limits<double>::infinity());
+    }
+
+    //helper function in the DP
+    auto considerbest = [&](dp_hier_val& left_cut, dp_hier_val& right_cut) {
+	double sum_cost = left_cut.sumload + right_cut.sumload;
+
+	if (sum_cost < ret.sumload) {
+	  //	  std::cout<<"new sol"<<std::endl;
+
+	  ret.sol.clear();
+	  std::copy(left_cut.sol.begin(),
+		    left_cut.sol.end(),
+		    std::back_inserter(ret.sol));
+	  std::copy(right_cut.sol.begin(),
+		    right_cut.sol.end(),
+		    std::back_inserter(ret.sol));
+	  ret.maxload = std::max(left_cut.maxload, right_cut.maxload);
+	  ret.sumload = left_cut.sumload + right_cut.sumload;
+	}	
+    };
+
+    auto potential = [&](dp_hier_val& left_cut) {
+      return left_cut.sumload < ret.sumload;
+    };
+    
+    //all ycut
+    for (int p=1; p<nbparts; ++p) {
+      //how many parts on the left
+
+      //cutting y?
+      for (index cut = 0+param.ystep; cut <Ymax-1; cut += param.ystep) {
+	//cut is the last
+	
+	auto left_cut = partition_jaggedY_over_rec (param, inst,
+						    Xmin, Xmax, cut,
+						    p, maxloadallowed); //left is a recursive jagged cut
+
+	if (!potential(left_cut))
+	  continue;
+
+	auto right_cut = partition_jaggedZ(param, inst, Xmin, Xmax, cut, Ymax, nbparts-p, maxloadallowed);
+
+	considerbest(left_cut, right_cut);	
+      }      
+    }
+
+    //no ycut solution
+    {
+      auto right_cut = partition_jaggedZ(param, inst, Xmin, Xmax, 0, Ymax, nbparts, maxloadallowed);
+
+	//fake to reuse considerbest
+	dp_hier_val left_cut;
+	left_cut.maxload=0;
+	left_cut.sumload=0;
+
+	considerbest(left_cut, right_cut);
+    }
+    
+    //save solution and return it
+    param.memo_l3[k] = ret;
+    
+    return ret;    
+  }
+
+  
   dp_hier_val partition_jaggedY(dp_jagged_parameters& param,
 				const stkde::instance& inst,
 				index Xmin,
@@ -75,25 +281,10 @@ namespace stkde{
       return it->second;
     }
 
-
-    
-    dp_hier_val ret;
-    
-    //naive solution: one box
-    {
-      stkde::voxelbox vb(Xmin, Xmax,
-			 0, c.voxY,
-			 0, c.voxT);
-          
-      ret.sol.push_back(vb);
-      ret.maxload = cost_of_box(param, vb, inst);
-      ret.sumload = ((ret.maxload <= maxloadallowed)?ret.maxload : std::numeric_limits<double>::infinity());
-    }
-
-
-    //std::cout<<"jaggedY "<<Xmin<<" : "<<Xmax<<" "<<nbparts<<" is worth "<<ret.sumload<<std::endl;
-    
-
+    auto ret = partition_jaggedY_over_rec (param, inst,
+					   Xmin, Xmax, c.voxY,
+					   nbparts, maxloadallowed);
+        
     //save solution and return it
     param.memo_l2[k] = ret;
     
